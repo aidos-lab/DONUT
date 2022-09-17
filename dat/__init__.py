a7be0ea6235ee4e7e7545595a044f97db02f3919
Bastian Rieck <bastian@rieck.me>
2022-09-17 15:27:56 +0200

    Added test endpoint

--- dat/__init__.py
+++ dat/__init__.py
@@ -1,6 +1,7 @@
 """Main Flask module."""
 
 from flask import Flask
+from flask import render_template
 
 
 def create_app(test_config=None):
@@ -8,8 +9,12 @@ def create_app(test_config=None):
 
     print(app.instance_path)
 
-    @app.route('/')
+    @app.route("/")
     def index():
-        return 'Hello, World!'
+        return "Hello, World!"
+
+    @app.route("/about")
+    def about():
+        return render_template("about.html")
 
     return app

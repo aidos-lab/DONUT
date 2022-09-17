d3f3242b441e4848ed33567c6e4e7a8459f3bf5a
Bastian Rieck <bastian@rieck.me>
2022-09-17 15:33:29 +0200

    Fixed template call for index

--- dat/__init__.py
+++ dat/__init__.py
@@ -11,7 +11,7 @@ def create_app(test_config=None):
 
     @app.route("/")
     def index():
-        return "Hello, World!"
+        return render_template("index.html", data=[])
 
     @app.route("/about")
     def about():

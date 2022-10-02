b0f2781c2abc5e347360c03d930624394aa886e3
Bastian Rieck <bastian@rieck.me>
2022-10-02 18:35:51 +0200

    Showing execution time

--- dat/__init__.py
+++ dat/__init__.py
@@ -1,5 +1,7 @@
 """Main Flask module."""
 
+import datetime
+
 from flask import Flask
 from flask import render_template
 from flask import request
@@ -11,10 +13,13 @@ DATABASE_DIR = "data/"
 
 
 def create_app(test_config=None):
+    """Create main application."""
     app = Flask(__name__)
 
     @app.route("/", methods=["GET", "POST"])
     def index():
+        start = datetime.datetime.now()
+
         if request.method == "POST":
             query = request.form["query"]
         else:
@@ -23,7 +28,18 @@ def create_app(test_config=None):
         matches = search(DATABASE_DIR, query)
         data = matches
 
-        return render_template("index.html", data=data, query=query)
+        if matches:
+            duration = datetime.datetime.now() - start
+            duration = duration.total_seconds()
+        else:
+            duration = None
+
+        return render_template(
+            "index.html",
+            data=data,
+            query=query,
+            duration=duration,
+        )
 
     @app.route("/view/<int:identifier>/", methods=["GET"])
     def view(identifier):
